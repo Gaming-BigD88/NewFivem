0c767521c16e638be36885632ab62fd52d9264af
Gaming-BigD <156423983+Gaming-BigD88@users.noreply.github.com>
2024-10-09 20:44:16 +0200

    gs

--- resources/[esx_addons]/esx_allowlist/locales/de.lua
+++ resources/[esx_addons]/esx_allowlist/locales/de.lua
@@ -1,5 +1,5 @@
 Locales['de'] = {
-    ['allowlist_check']     = 'Überprüfe ob sie auf der Allowlist sind...',
+    ['allowlist_check']     = 'Überprüfe ob sie auf der Allowlist sind....',
     ['not_allowlisted']     = 'Sie müssen auf der Allowlist sein, um diesem Server beizutreten!',
     ['allowlist_empty']     = 'Für diesen Server sind keine allowlists gespeichert.',
     ['license_missing']     = 'Error: Ihr Identifier fehlt.',
